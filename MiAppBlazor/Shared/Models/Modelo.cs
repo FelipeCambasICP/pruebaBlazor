8e4d2b057274c0612eb8c4d209ceba52ea05727f
felipe.cambas <felipe.cambas@icp.es>
2021-12-10 17:45:13 +0100

    first commit

---
+++ MiAppBlazor/Shared/Models/Modelo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiAppBlazor.Shared.Models
+{
+    public class Modelo
+    {
+        public int page { get; set; }
+        public int per_page { get; set; }
+        public int total { get; set; }
+        public int total_pages { get; set; }
+    
+        public List<Contact> data { get; set; }
+
+
+        public Modelo()
+        {
+            data = new List<Contact>();
+        }
+    }
+}
